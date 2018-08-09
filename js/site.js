b39fe46169e306fc90ba21b9fe9a2f9f5c3df17f
delusional-irony <martrexwl@gmail.com>
2018-08-09 13:00:33 -0400

    Initial commit

--- js/site.js
+++ js/site.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	var hako = document.getElementsByClassName('hako');
 	for (i=1; i<hako.length; i++){
-		alert(hako[i].innerHTML.replace(/\s/g,''));
-		if(window.location.href.indexOf(hako[i].innerHTML.trim()) > -1) {
+		if(window.location.href.indexOf(hako[i].innerHTMLreplace(/\s/g,'')) > -1) {
 			hako[0].classList.remove("cur");
 			hako[i].classList.add("cur");
 		}
