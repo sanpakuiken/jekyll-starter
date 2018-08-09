ab2d7eab805763bd85b396494d8f9239bc61c714
delusional-irony <martrexwl@gmail.com>
2018-08-09 12:44:52 -0400

    Initial commit

--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	var hako = document.getElementsByClassName('hako');
 	for (i=1; i<hako.length; i++){
+		alert(hako[i].innerHTML.trim());
 		if(window.location.href.indexOf(hako[i].innerHTML.trim()) > -1) {
 			hako[0].classList.remove("cur");
 			hako[i].classList.add("cur");
-			alert(hako[i].innerHTML.trim());
 		}
 	}
 });
